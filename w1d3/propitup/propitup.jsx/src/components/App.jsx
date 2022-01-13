1066dd0df3ff0fc0738ccaec8f50bb0e15b0074d
John Doe <s.maeflower@gmail.com>
2022-01-12 21:07:44 -0600

    new assign push

---
+++ w1d3/propitup/propitup.jsx/src/components/App.jsx
@@ -0,0 +1,16 @@
+import PersonCard from "./PersonCard";
+
+function App() {
+    return (
+        <div>
+            <PersonCard firstName = {"Buster"} lastName = {"Baxter"} age = {10} hairColor = {"white"}></PersonCard>
+            <PersonCard firstName = {"Arthur"} lastName = {"Read"} age = {10} hairColor = {"brown"}></PersonCard>
+            <PersonCard firstName = {"D.W."} lastName = {"Read"} age = {6} hairColor = {"brown"}></PersonCard>
+            <PersonCard firstName = {"Binky"} lastName = {"Barnes"} age = {10} hairColor = {"brown"}></PersonCard>
+            
+        </div>
+    );
+}
+
+
+export default App;
